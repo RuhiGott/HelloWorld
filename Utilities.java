d48e7c350dbf5605a7096099f60bcde7ea3d1da5
RuhiGott <ruhigottu@gmail.com>
2023-09-05 18:08:38 -0700

    update Utilities.java

--- Utilities.java
+++ Utilities.java
@@ -1,5 +1,15 @@
 public class Utilities{
-  public static void printHello(){
-    System.out.println("Hellow ord");
+  public static void printHello(String str){
+    if (str.equalsIgnoreCase("hello")){
+      System.out.println("hello, world!");
+    }
+
+    else if (str.equalsIgnoreCase("goodbye")) {
+      System.out.println("goodbye, cruel world");
+    }
+
+    else{
+      System.out.println("other message");
+    }
   }
 }
