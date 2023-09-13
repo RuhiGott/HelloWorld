8dc6fcde0a1b514cbd47e42d2acbe97414a857e8
EddieY <48185035+Op2z@users.noreply.github.com>
2023-09-13 10:04:24 -0700

    Update Utilities.java
    add parameter functionality

--- Utilities.java
+++ Utilities.java
@@ -1,5 +1,9 @@
 public class Utilities{
-  public static void printHello(){
-    System.out.println("Hellow ord");
+  public static void printHello(String input){
+    if (input.equalsIgnoreCase("Hello"){
+      System.out.println("Hello World");
+    }
+    else {
+      System.out.println("Goodbye World");
   }
 }
